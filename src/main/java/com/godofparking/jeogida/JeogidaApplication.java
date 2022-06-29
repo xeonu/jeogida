6aa130a6f121fe770c57aa1886f1830f34ce8e05
TOtooRo <35839248+xeonu@users.noreply.github.com>
2022-06-29 19:10:59 +0900

    Initial commit

---
+++ src/main/java/com/godofparking/jeogida/JeogidaApplication.java
@@ -0,0 +1,13 @@
+package com.godofparking.jeogida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeogidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeogidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godofparking/jeogida/JeogidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godofparking.jeogida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeogidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
